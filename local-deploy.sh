b57268eb8604ff128b2679256e14845949dc1f9a
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-01-23 23:52:39 +0100

    fix: make it possible to call the deploy script from other folder

--- local-deploy.sh
+++ local-deploy.sh
@@ -3,14 +3,21 @@
 function up() {
     ./scripts/init-postgres.sh
     # TODO: bring up all services
-    docker compose up -d --build rabbitmq-broker cassandra-db cassandra-init location-service gateway
+    docker compose up -d --build rabbitmq-broker postgres-db cassandra-db cassandra-init location-service gateway
+    cleanup
+}
+
+function cleanup() {
+    rm -rf .tmp
 }
 
 function down() {
     docker compose down
-    rm -rf .tmp
+    cleanup
 }
 
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+cd $SCRIPT_DIR
 if [ "$1" == "up" ]; then
     up
 elif [ "$1" == "down" ]; then
--- scripts/init-postgres.sh
+++ scripts/init-postgres.sh
@@ -5,13 +5,14 @@ INIT_SCRIPTS=(
     "user-service/refs/heads/main/init.sql"
     "/notification-service/refs/heads/main/storage/src/main/resources/ddl-scripts/create-tables.sql"
 )
-OUT_DIR=".tmp/"
+TMP_DIR=".tmp/"
+echo "Temporary dir: $TMP_DIR"
 
-if [ ! -d "$OUT_DIR" ]; then
-    mkdir $OUT_DIR
+if [ ! -d "$TMP_DIR" ]; then
+    mkdir $TMP_DIR
 fi
 
 for script in "${INIT_SCRIPTS[@]}"; do
     echo "Downloading $script..."
-    wget --directory-prefix=$OUT_DIR $POSITION_PAL_ORG/$script
+    wget --directory-prefix=$TMP_DIR $POSITION_PAL_ORG/$script
 done
